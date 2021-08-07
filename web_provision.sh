473c85a207c60378ae2fdd53323e11ff22d7e0e2
SungYeonKim <60651715+liz0904@users.noreply.github.com>
2021-08-07 20:43:01 +0900

    Update web_provision.sh

--- web_provision.sh
+++ web_provision.sh
@@ -6,9 +6,6 @@ rm -f postinstall.sh
 
 #install apache and php
 sudo apt-get update
-sudo apt-get install -y apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-client
+sudo apt-get install -y apache2 php libapache2-mod-php 
+sudo apt-get install -y php-mysql mysql-client
 sudo service apache2 restart
-
-echo "extension=php_mysql.so" >> /etc/php5/apache2/php.ini
-echo cd / >> /home/vagrant/.bashrc
-echo "Hello World from WebBox!"
